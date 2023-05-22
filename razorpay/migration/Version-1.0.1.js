3b6267f6fe4458f44e30980508032b9f94da81b5
= <=>
2023-05-22 12:44:40 +0530

    [JIRA-1] Clean Up

--- razorpay/migration/Version-1.0.1.js
+++ razorpay/migration/Version-1.0.1.js
@@ -2,6 +2,5 @@ const { execute } = require('@evershop/postgres-query-builder');
 
 // eslint-disable-next-line no-multi-assign
 module.exports = exports = async (connection) => {
-    console.log(connection)
   await execute(connection, `ALTER TABLE "order" ADD COLUMN razorpay_order_id VARCHAR(50) DEFAULT NULL;`);
 };
