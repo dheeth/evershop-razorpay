3532c77d5781f657a29a748cc0bd3a63b3584d66
= <=>
2024-06-15 10:22:34 +0530

    fix cart issue

--- razorpay/bootstrap.js
+++ razorpay/bootstrap.js
@@ -1,20 +1,29 @@
-const { Cart } = require('@evershop/evershop/src/modules/checkout/services/cart/Cart');
+const { addProcessor } = require('@evershop/evershop/src/lib/util/registry');
 const { getSetting } = require('@evershop/evershop/src/modules/setting/services/setting');
 
-module.exports = () => {
-  Cart.addField('payment_method', async function resolver(previousValue) {
-    const paymentMethod = this.dataSource?.payment_method ?? null;
-    if (paymentMethod !== 'razorpay') {
-      return previousValue;
-    } else {
-      // Validate the payment method
-      const razorpayStatus = await getSetting('razorpayPaymentStatus');
-      if (parseInt(razorpayStatus, 10) !== 1) {
-        return previousValue;
-      } else {
-        delete this.errors.payment_method;
-        return paymentMethod;
-      }
-    }
-  });
-};
+  module.exports = () => {
+    addProcessor('cartFields', (fields) => {
+      fields.push({
+        key: 'payment_method',
+        resolvers: [
+          async function resolver(paymentMethod) {
+            // Do nothing if the payment method is not paypal
+            if (paymentMethod !== 'paypal') {
+              return paymentMethod;
+            } else {
+              // Validate the payment method
+              const paypalStatus = await getSetting('paypalPaymentStatus');
+              if (parseInt(paypalStatus, 10) !== 1) {
+                return null;
+              } else {
+                this.setError('payment_method', undefined);
+                return paymentMethod;
+              }
+            }
+          }
+        ]
+      });
+      return fields;
+    });
+  };
+  
